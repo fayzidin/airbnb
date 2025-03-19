53b64a86018522c0e429d6ad098f3fc65615fba5
fayzidin_sayliev <fayzidin_sayliev@epam.com>
2025-03-19 17:45:19 +0500

    yaml dags

---
+++ dags/airflow_listings_hive.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocSubmitHiveJobOperator,
+    DataprocSubmitJobOperator,
+)
+from airflow.utils.dates import days_ago
+
+# Configuration Variables
+PROJECT_ID = "airbnb-448411"
+REGION = "us-central1"  # Change based on your cluster region
+CLUSTER_NAME = "cluster-a7d1"  # Use your existing Dataproc cluster name
+BUCKET_NAME = "airbnb-chicago"  # GCS bucket where scripts are stored
+HIVE_SCRIPT_URI = f"gs://airbnb-chicago/logic/listingsToParHive.hql"
+
+# Default DAG arguments
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": days_ago(1),
+    "retries": 1,
+}
+
+# Define the DAG
+with DAG(
+    "listings_to_par_hive",
+    default_args=default_args,
+    schedule_interval=None,  # Trigger manually or define a schedule
+    catchup=False,
+) as dag:
+
+    # Submit Hive Job to the Existing Cluster
+    submit_hive_listings_job = DataprocSubmitHiveJobOperator(
+        task_id="submit_hive_listings_job",
+        project_id=PROJECT_ID,
+        region=REGION,
+        cluster_name=CLUSTER_NAME,
+        query_uri=HIVE_SCRIPT_URI,
+    )
+
+    # Define DAG dependencies
+    submit_hive_listings_job
---
+++ dags/airflow_reviews_pyspark.py
@@ -0,0 +1,44 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocSubmitHiveJobOperator,
+    DataprocSubmitJobOperator,
+)
+from airflow.utils.dates import days_ago
+
+# Configuration Variables
+PROJECT_ID = "airbnb-448411"
+REGION = "us-central1"  # Change based on your cluster region
+CLUSTER_NAME = "cluster-a7d1"  # Use your existing Dataproc cluster name
+BUCKET_NAME = "airbnb-chicago"  # GCS bucket where scripts are stored
+PYSPARK_SCRIPT_URI = f"gs://airbnb-chicago/logic/reviewsLogicToParquet.py"
+
+# Default DAG arguments
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": days_ago(1),
+    "retries": 1,
+}
+
+# Define the DAG
+with DAG(
+    "reviews_to_par_pyspark",
+    default_args=default_args,
+    schedule_interval=None,  # Trigger manually or define a schedule
+    catchup=False,
+) as dag:
+
+    # Submit PySpark Job to the Existing Cluster
+    submit_pyspark_reviews_job = DataprocSubmitJobOperator(
+        task_id="submit_pyspark_reviews_job",
+        project_id=PROJECT_ID,
+        region=REGION,
+        job={
+            "reference": {"project_id": PROJECT_ID},
+            "placement": {"cluster_name": CLUSTER_NAME},
+            "pyspark_job": {"main_python_file_uri": PYSPARK_SCRIPT_URI},
+        },
+    )
+
+    # Define DAG dependencies
+    submit_pyspark_reviews_job
